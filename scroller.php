14b8062264b6ed94837ed05ba4b0116f6beb0d31
georgecesar <giorgio.cesaroni@me.com>
2020-10-05 13:52:35 -0400

    Added Plugin URI for GitHub Updater

--- scroller.php
+++ scroller.php
@@ -7,6 +7,7 @@
  * Author:          Giorgio Cesaroni
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/georgecesar/scroller-plugin
  * Text Domain:     scroller
  *
  * @package         create-block
