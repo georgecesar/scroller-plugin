edc9d8a6079f05301aec632f73029cb2600a06de
georgecesar <giorgio.cesaroni@me.com>
2020-10-12 15:25:57 -0400

    Version number

--- scroller.php
+++ scroller.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Scroller
  * Description:     A simple gallery scoller for Electric Air Studios' website.
- * Version:         1.1
+ * Version:         1.2
  * Author:          Giorgio Cesaroni
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
