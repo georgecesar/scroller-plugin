4aa9784a21129d5bed055be4d8f7ff431259290e
georgecesar <giorgio.cesaroni@me.com>
2020-11-06 12:15:58 -0500

    Transition on dot change

--- scroller.php
+++ scroller.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Scroller
  * Description:     A simple gallery scoller for Electric Air Studios' website.
- * Version:         1.2
+ * Version:         1.3
  * Author:          Giorgio Cesaroni
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
@@ -21,53 +21,53 @@
  */
 function create_block_scroller_block_init()
 {
-	$dir = dirname(__FILE__);
+  $dir = dirname(__FILE__);
 
-	$script_asset_path = "$dir/build/index.asset.php";
-	if (!file_exists($script_asset_path)) {
-		throw new Error(
-			'You need to run `npm start` or `npm run build` for the "create-block/scroller" block first.'
-		);
-	}
-	$index_js     = 'build/index.js';
-	$script_asset = require($script_asset_path);
-	wp_register_script(
-		'create-block-scroller-block-editor',
-		plugins_url($index_js, __FILE__),
-		$script_asset['dependencies'],
-		$script_asset['version']
-	);
-	wp_set_script_translations('create-block-scroller-block-editor', 'scroller');
+  $script_asset_path = "$dir/build/index.asset.php";
+  if (!file_exists($script_asset_path)) {
+    throw new Error(
+      'You need to run `npm start` or `npm run build` for the "create-block/scroller" block first.'
+    );
+  }
+  $index_js     = 'build/index.js';
+  $script_asset = require($script_asset_path);
+  wp_register_script(
+    'create-block-scroller-block-editor',
+    plugins_url($index_js, __FILE__),
+    $script_asset['dependencies'],
+    $script_asset['version']
+  );
+  wp_set_script_translations('create-block-scroller-block-editor', 'scroller');
 
-	// $editor_css = 'build/index.css';
-	// wp_register_style(
-	// 	'create-block-scroller-block-editor',
-	// 	plugins_url( $editor_css, __FILE__ ),
-	// 	array(),
-	// 	filemtime( "$dir/$editor_css" )
-	// );
+  // $editor_css = 'build/index.css';
+  // wp_register_style(
+  // 	'create-block-scroller-block-editor',
+  // 	plugins_url( $editor_css, __FILE__ ),
+  // 	array(),
+  // 	filemtime( "$dir/$editor_css" )
+  // );
 
-	// $style_css = 'build/style-index.css';
-	// wp_register_style(
-	// 	'create-block-scroller-block',
-	// 	plugins_url( $style_css, __FILE__ ),
-	// 	array(),
-	// 	filemtime( "$dir/$style_css" )
-	// );
+  // $style_css = 'build/style-index.css';
+  // wp_register_style(
+  // 	'create-block-scroller-block',
+  // 	plugins_url( $style_css, __FILE__ ),
+  // 	array(),
+  // 	filemtime( "$dir/$style_css" )
+  // );
 
-	register_block_type('create-block/scroller', array(
-		'editor_script' => 'create-block-scroller-block-editor',
-		'editor_style'  => 'create-block-scroller-block-editor',
-		'style'         => 'create-block-scroller-block',
-	));
+  register_block_type('create-block/scroller', array(
+    'editor_script' => 'create-block-scroller-block-editor',
+    'editor_style'  => 'create-block-scroller-block-editor',
+    'style'         => 'create-block-scroller-block',
+  ));
 }
 
 add_action('init', 'create_block_scroller_block_init');
 
 function enqueue_assets()
 {
-	wp_enqueue_script("scroller", plugins_url("/src/scroller.js", __FILE__));
-	wp_enqueue_style("scroller", plugins_url("/src/scroller.css", __FILE__));
+  wp_enqueue_script("scroller", plugins_url("/src/scroller.js", __FILE__));
+  wp_enqueue_style("scroller", plugins_url("/src/scroller.css", __FILE__));
 }
 
 add_action('enqueue_block_assets', 'enqueue_assets');
